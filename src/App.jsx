2faae812625202e565fed5789e6a348177b86f53
Soumya Sekhar Mahanti <soumyasekharmahanti@gmail.com>
2024-03-12 10:40:53 +0530

    hrere

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1> Sir here is my webpage</h1>
+        <img src="src/assets/Image.jpeg" alt="" height={"300px"} />
+      </div>
+    </>
+  );
+}
+
+export default App;
